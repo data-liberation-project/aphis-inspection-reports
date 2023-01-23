02c9d6117912387eedc9ec11638ff63f190e96c3
Ben Welsh <b@palewi.re>
2023-01-23 03:27:09 +0000

    Mark new records with a timestamp

--- scripts/01-refresh-inspection-list.py
+++ scripts/01-refresh-inspection-list.py
@@ -1,6 +1,7 @@
 import csv
+from datetime import datetime, timezone
 
-from lib.aphis import add_hash_ids, deduplicate, fetch, iter_fetch_all, write_results
+from lib.aphis import add_hash_ids, deduplicate, fetch, get_sort_key, iter_fetch_all, write_results
 
 
 def main() -> None:
@@ -8,13 +9,32 @@ def main() -> None:
     with open("data/fetched/inspections-search-total.txt", "w") as f:
         f.write(str(search_total))
 
-    latest = deduplicate(iter_fetch_all({}, raise_size_error=False))
+    latest = add_hash_ids(deduplicate(iter_fetch_all({}, raise_size_error=False)))
     write_results(latest, "data/fetched/inspections-latest.csv")
 
-    # Update CSV containing all historically-observed inspections
-    previous = list(csv.DictReader(open("data/fetched/inspections.csv")))
-    combined = add_hash_ids(deduplicate(latest + previous))
-    write_results(combined, "data/fetched/inspections.csv")
+    # Read in historically-observed inspections
+    with open("data/fetched/inspections.csv", 'r') as fp:
+        lookup = dict((d['hash_id'], d) for d in csv.DictReader(fp))
+
+    # Get the current time for datestamping
+    now = datetime.now(timezone.utc)
+
+    # Loop through all of the newly scraped data
+    for d in latest:
+        # Get its key
+        key = d['hash_id']
+        # If the record is new
+        if key not in lookup:
+            # Mark the time
+            d['discovered'] = now
+            # Add it to the lookup
+            lookup[key] = d
+
+    # Convert the lookup back into a list
+    merged = sorted(lookup.values(), key=get_sort_key)
+
+    # Write it out
+    write_results(merged, "data/fetched/inspections.csv")
 
 
 if __name__ == "__main__":
