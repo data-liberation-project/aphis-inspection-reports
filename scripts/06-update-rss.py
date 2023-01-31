4c188ec952d57ecf7cba8d643c7cdb67489dbc9f
Jeremy Singer-Vine <jsvine@gmail.com>
2023-01-31 14:39:32 -0500

    Appease the linters

--- scripts/06-update-rss.py
+++ scripts/06-update-rss.py
@@ -1,7 +1,7 @@
+import csv
 from datetime import datetime
 from pathlib import Path
 
-import csv
 from feedgen.feed import FeedGenerator
 
 # Set directories we'll use
@@ -16,32 +16,36 @@ def main() -> None:
 
     # Parse dates
     for r in fetched_data:
-        r['web_discovered'] = datetime.fromisoformat(r['web_discovered'])
+        r["web_discovered"] = datetime.fromisoformat(r["web_discovered"])
 
     # Sort reverse chron
-    sorted_data = sorted(fetched_data, key=lambda x: x['web_discovered'], reverse=True)
+    sorted_data = sorted(fetched_data, key=lambda x: x["web_discovered"], reverse=True)
 
     # Slice the most recent 50 for inclusion in our feed
     feed_data = sorted_data[:50]
 
     # Create our feed object
     fg = FeedGenerator()
-    fg.title('Lastest APHIS inpections')
+    fg.title("Lastest APHIS inpections")
     fg.link(href="https://github.com/data-liberation-project/aphis-inspection-reports")
-    fg.description("The latest inspections posted online by the U.S. Department of Agriculture's Animal and Plant Health Inspection Service")
+    fg.description(
+        "The latest inspections posted online by the U.S. Department of Agriculture's Animal and Plant Health Inspection Service"  # noqa: E501
+    )
 
     # Add our feed entries
     for row in reversed(feed_data):
         fe = fg.add_entry()
-        fe.id(row['web_hash_id'])
+        fe.id(row["web_hash_id"])
         fe.title(f"{row['web_legalName']} ({row['web_inspectionDate']})")
-        fe.link(href=row['doccloud_url'])
-        fe.description(f"""Critical: {row['web_critical']}
+        fe.link(href=row["doccloud_url"])
+        fe.description(
+            f"""Critical: {row['web_critical']}
 Not critical: {row['web_nonCritical']}
 Teachable moments: {row['web_teachableMoments']}
 
 Discovered on {row['web_discovered']}
-""")
+"""
+        )
 
     # Write it out
     fg.rss_file(DATA_DIR / "combined" / "latest-inspections.rss", pretty=True)
