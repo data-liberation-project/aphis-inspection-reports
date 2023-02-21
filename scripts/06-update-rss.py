7b0340462be8802f6a972d137afaedb3aef080b2
palewire <b@palewi.re>
2023-02-21 11:13:22 -0500

    Add an RSS file that's limited to critical inspections

--- scripts/06-update-rss.py
+++ scripts/06-update-rss.py
@@ -20,35 +20,49 @@ def main() -> None:
 
     # Sort reverse chron
     sorted_data = sorted(fetched_data, key=lambda x: x["discovered"], reverse=True)
-
-    # Slice the most recent 50 for inclusion in our feed
-    feed_data = sorted_data[:50]
-
-    # Create our feed object
-    fg = FeedGenerator()
-    fg.title("Lastest APHIS inpections")
-    fg.link(href="https://github.com/data-liberation-project/aphis-inspection-reports")
-    fg.description(
+    # Create our full feed
+    full_feed = FeedGenerator()
+    full_feed.title("Lastest APHIS inpections")
+    full_feed.link(href="https://github.com/data-liberation-project/aphis-inspection-reports")
+    full_feed.description(
         "The latest inspections posted online by the U.S. Department of Agriculture's Animal and Plant Health Inspection Service"  # noqa: E501
     )
-
     # Add our feed entries
-    for row in reversed(feed_data):
-        fe = fg.add_entry()
-        fe.id(row["hash_id"])
-        fe.title(f"{row['web_legalName']} ({row['web_inspectionDate']})")
-        fe.link(href=row["doccloud_url"])
-        fe.description(
-            f"""Critical: {row['web_critical']}
-Not critical: {row['web_nonCritical']}
-Teachable moments: {row['web_teachableMoments']}
+    for row in reversed(sorted_data[:50]):
+        # Add it to the full feed
+        full_entry = full_feed.add_entry()
+        _create_entry(full_entry, row)
 
-Discovered on {row['discovered']}
-"""
-        )
+    # Create our critical feed
+    critical_feed = FeedGenerator()
+    critical_feed.title("Lastest critical APHIS inpections")
+    critical_feed.link(href="https://github.com/data-liberation-project/aphis-inspection-reports")
+    critical_feed.description(
+        "The latest inspections with critical violations posted online by the U.S. Department of Agriculture's Animal and Plant Health Inspection Service"  # noqa: E501
+    )
+    # Only add critical inspections to the critical feed
+    critical_data = [d for d in sorted_data if int(d['web_critical']) > 0]
+    for row in reversed(critical_data[:50]):
+        critical_entry = critical_feed.add_entry()
+        _create_entry(critical_entry, row)
 
     # Write it out
-    fg.rss_file(DATA_DIR / "combined" / "latest-inspections.rss", pretty=True)
+    full_feed.rss_file(DATA_DIR / "combined" / "latest-inspections.rss", pretty=True)
+    critical_feed.rss_file(DATA_DIR / "combined" / "latest-critical-inspections.rss", pretty=True)
+
+
+def _create_entry(entry, data):
+    entry.id(data["hash_id"])
+    entry.title(f"{data['web_legalName']} ({data['web_inspectionDate']})")
+    entry.link(href=data["doccloud_url"])
+    entry.description(
+        f"""Critical: {data['web_critical']}
+Not critical: {data['web_nonCritical']}
+Teachable moments: {data['web_teachableMoments']}
+
+Discovered on {data['discovered']}
+"""
+    )
 
 
 if __name__ == "__main__":
